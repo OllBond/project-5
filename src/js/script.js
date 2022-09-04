fe8684489a8f53de831232ba82c0bbb25ad4a81a
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-09-04 21:51:36 +0300

    addsliderimg

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,16 @@ $(".carousel").slick({
     dots: true,
     speed: 1000,
     autoplay: true,
+    adaptiveHeight: true,
     autoplaySpeed: 2500,
     
+});
+
+import "slick-slider";
+$('.single-item').slick({
+     dots: true,
+    speed: 1000,
+    autoplay: tru,
+    autoplaySpeed: 3000,
+    arrows:false,
 });
