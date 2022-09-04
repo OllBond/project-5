30b8cae29cd8f1f11f7701b5023ef192341e3646
KateBabak <katebabak8@gmail.com>
2022-09-05 00:06:15 +0200

    working 4 form

--- src/js/_modal.js
+++ src/js/_modal.js
@@ -49,3 +49,21 @@
     document.body.classList.toggle("active-modal");
   }
 })();
+
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-buynow-open]"),
+    closeModalBtn: document.querySelector("[data-modal-buynow-close]"),
+    modal: document.querySelector("[data-modal-buynow]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("active-modal");
+  }
+})();
